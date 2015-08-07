dc45da9dd0478aff4fedf1c299b98969420ef6ff
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-08-07 18:31:33 +0530

    [WIP] authorize_user!

--- app/controllers/easy_roles/application_controller.rb
+++ app/controllers/easy_roles/application_controller.rb
@@ -1,4 +1,18 @@
 module EasyRoles
   class ApplicationController < ActionController::Base
+
+    before_filter :authorize_user!
+
+    def authorize_user!
+      if current_user.blank?
+        raise CustomExceptions::NotAuthorizedUser
+      else
+        easy_role_user = User.find(current_user.id)
+        unless current_user.authorize_to_perform_action?(params[:controller], params[:action], params[:group])
+          raise CustomExceptions::NotAuthorizedUser
+        end
+      end
+    end
+
   end
 end
