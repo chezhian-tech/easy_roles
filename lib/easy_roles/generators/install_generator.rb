f567ef77c8f9a514882b568401308d458ba9b3f2
Selva Chezhian <selvachezhian.labam@gmail.com>
2015-08-11 14:56:34 +0530

    [WIP] installation steps with rails generators

--- lib/easy_roles/generators/install_generator.rb
+++ lib/easy_roles/generators/install_generator.rb
@@ -4,6 +4,11 @@ module EasyRoles
   module Generators
     class InstallGenerator < Rails::Generators::Base
 
+      desc 'change User model\'s base class to engine\'s User model'
+      def change_user_base_class
+        gsub_file 'app/models/user.rb', 'class User < ActiveRecord::Base', 'class User < EasyRoles::User'
+      end
+
     end
   end
 end
